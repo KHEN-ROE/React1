f82a723224dd68e7feb67ba947986147d4f4fd65
KEN <shdnrnjs@gmail.com>
2023-02-06 01:28:48 +0900

    2/6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';//index.js에서 App임포트하고
-import Frcst from './04/Frcst';
+// import Frcst from './04/Frcst';
 // import Mydiv from './03/Mydiv';
 // import Taccident from './05/Taccident';
 // import Box from './02/Box';
-import Example from './03/Example';
+// import Example from './03/Example';
+import Taccident1 from './05/Taccident1';
+
 
 const App = () => {//return은 하나의 태그만 할 수 있다. 여러개 하고 싶으면 부모태그로 감싸면됨
   return (
-      <Frcst />
+      // <Frcst />
       //  <Mydiv />
       // <Taccident />
       // <Box />
       //  <Example />
+      <Taccident1 />
   );
 }
 
