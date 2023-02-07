7ae60500f4b43befa4cade3d5c532baff9cb18b2
KEN ROE <shdnrnjs@gmail.com>
2023-02-07 11:24:06 +0900

    2/7마무리

--- src/06/GalMain.js
+++ src/06/GalMain.js
@@ -3,6 +3,9 @@ import GalC1 from "./GalC1";
 import GalC2 from "./GalC2";
 import Ghead from "./Ghead";
 
+//순서 - GalC1에서 타이틀들을 화면에 출력하고 그 후에 useState를 선언한다. 클릭했을 때 호출되는 함수를 선언한다. 그 함수에서 setSelc1을 호출하여 클릭될 때마다 useState를 변화시키게 하고(클릭한 배열의 속성을 selc1에 저장) 재랜더링되게한다.
+//이제 랜더링될 때마다 연산을 수행할 useEffect를 선언한다. useEffect에서는 선택한 selc1이 data1배열의 galTilte과 일치하면 data1배열에서 그 타이틀이 있는 항목을 selData에 저장한다.
+//이제 selData에 data1의 상세 내용들이 저장되었고 GalC2에 넘겨준다. GalC2 에서는 selData를 받아서 화면에 출력한다.(map함수를 쓰든, return문 안에 seldata.galTitle 이런식으로 하든)
 
 const GalMain = ({c1, c2, data1}) => {
     
@@ -14,9 +17,9 @@ const GalMain = ({c1, c2, data1}) => {
 
     
 
-    // useEffect(()=>{//타이틀들을 클릭할 때마다 state가 변화하게 되는 것이고, 이에 따라 재 랜더링된다. 랜더링이 될 때마다 useEffect는 실행된다. 즉 여기서는 selc1이 변화할 때마다 이 useEffect가 실행된다 
-    //     console.log("selc1", selc1)
-    // },[selc1])
+    useEffect(()=>{//타이틀들을 클릭할 때마다 state가 변화하게 되는 것이고, 이에 따라 재 랜더링된다. 랜더링이 될 때마다 useEffect는 실행된다. 즉 여기서는 selc1이 변화할 때마다 이 useEffect가 실행된다 
+        console.log("selc1", selc1)
+    },[selc1])
 
     useEffect(()=>{//selc1(타이틀)이 클릭될 때마다 if문 수행. 즉, 타이틀이 클릭될 때마다 data1 배열에 대해서 필터함수 발동.galTitle과 클릭한 Selc1이 일치하면 selData에 클릭한 타이틀 저장 
         if(selc1){
@@ -37,8 +40,8 @@ const GalMain = ({c1, c2, data1}) => {
             </div>
 
             <div className="main">
-                <GalC1 c1={c1} setSelc1={setSelc1} />
-                <GalC2 selData={selData} />
+                <GalC1 c1={c1} selc1={selc1} setSelc1={setSelc1} />
+                {selData && <GalC2 selData={selData} />}
             </div>
         </div>
     );
