aa6b8e89403dbb3f0a518350f4da9b91a64a9c1e
KEN ROE <shdnrnjs@gmail.com>
2023-02-17 14:07:41 +0900

    비동기 다시

--- src/11/BoxofficDetail.js
+++ src/11/BoxofficDetail.js
@@ -7,22 +7,27 @@ const BoxofficDetail = ({mvcd}) => {
     console.log("BoxofficeDetail",mvcd)
 
     const getData = async() => {
-        let url = `https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=f5eef3421c602c6cb7ea224104795888&movieCd=${mvcd}`;
+        let url = 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=f5eef3421c602c6cb7ea224104795888&'
+        url = url + `movieCd =${mvcd}`;
 
         try{
             const resp = await fetch(url);
             const data = await resp.json() ;
             
+            console.log("url", url)
             //object
             let temp = data.movieInfoResult.movieInfo ;
-            let showItem = {'영화명': 'movieNm', '개봉일' : 'openDt', '제작상태' : 'prdtStatNm', '영화구분':'typeNm'}
+            let showItem = {'영화명': 'movieNm',
+                            '개봉일' : 'openDt',
+                            '제작상태' : 'prdtStatNm',
+                            '영화구분':'typeNm'}
 
             if(mvcd){
                 let tag=[];
             for(let[k,v] of Object.entries(showItem)){
-                tag.push(<div>
-                    <span>{k}</span>
-                    <span>{temp[v][0].peopleNm}</span>
+                tag.push(<div className="dtdiv" key={k}>
+                    <span className="dtsp1">{k}</span>
+                    <span className="stsdp2">{temp[v]}</span>
                 </div>)
                 
             }
